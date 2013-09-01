df1539708525469ed6e1d46036899ca4ac6f9abc
Ernado <ernado@cygame.ru>
2013-09-01 12:24:02 +0400

    verison bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webutils',
-    version='0.2.1',
+    version='0.3',
     packages=['webutils'],
     include_package_data=True,
     license='BSD License',  # example license
