0dea9a4c50d98fb9ef39380194df1cc24aa53fe6
Ernado <ernado@cygame.ru>
2013-09-01 14:47:49 +0400

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webutils',
-    version='0.3.7',
+    version='0.3.8',
     packages=['webutils', 'webutils.captcha', 'webutils.watermarker'],
     include_package_data=True,
     license='BSD License',  # example license
