2bc350a243ed3bed91818e11c1d5e5f909afd7fb
Ernado <ernado@cygame.ru>
2013-09-01 13:16:20 +0400

    fixed bug

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-webutils',
-    version='0.3.2',
+    version='0.3.3',
     packages=['webutils', 'webutils.captcha', 'webutils.watermarks'],
     include_package_data=True,
     license='BSD License',  # example license
